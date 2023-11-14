b636cf72bf3d43cecf8595d8fa4338cc3bf8e61d
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-14 11:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
-	if(str1 == "" || str2 == ""){
-	    return '';
+	if(s1 == "" || s2 == ""){
+	    return "";
     }
-    let f = str1.charAt(0);
-    let s = str2.charAt(0);
+    let f = s1.charAt(0);
+    let s = s2.charAt(0);
     return f.concat(s);
 }
+
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
